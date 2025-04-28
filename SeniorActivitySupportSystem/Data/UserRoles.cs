98b4c3229e8a3f0f34e55d2a347386cdbe4021f3
Maksymilian Zmuda <maksymilianzmuda02@gmail.com>
2025-04-29 00:51:46 +0200

    Identity framework and migrations (#4)

---
+++ SeniorActivitySupportSystem/Data/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace SeniorActivitySupportSystem.Data
+{
+    public static class UserRoles
+    {
+        public const string Admin = "admin";
+        public const string User = "user";
+    }
+}
--- SeniorActivitySupportSystem/Models/AppUser.cs
+++ SeniorActivitySupportSystem/Models/AppUser.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SeniorActivitySupportSystem.Models
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
-        [Key]
-        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Bio { get; set; }
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
         public Address Address { get; set; }
 
         public ICollection<SportEvent> SportEvents { get; set; }
